9b9220c9f4c55f2961638af169a79a673f7b1b50
Rodion Grishaev <r.grishaev@softteco.com>
2025-01-16 19:01:07 +0300

    Add CommonExceptionFilter.

---
+++ src/common/exception-filters/common-exception.filter.ts
@@ -0,0 +1,39 @@
+import {
+  ExceptionFilter,
+  Catch,
+  ArgumentsHost,
+  HttpException,
+  HttpStatus,
+} from '@nestjs/common';
+import { HttpAdapterHost } from '@nestjs/core';
+import { ErrorMessages } from '../constants/error-messages';
+
+@Catch()
+export class CommonExceptionFilter implements ExceptionFilter {
+  constructor(private readonly httpAdapterHost: HttpAdapterHost) {}
+
+  catch(exception: unknown, host: ArgumentsHost): void {
+    const { httpAdapter } = this.httpAdapterHost;
+    const ctx = host.switchToHttp();
+
+    const httpStatus =
+      exception instanceof HttpException
+        ? exception.getStatus()
+        : HttpStatus.INTERNAL_SERVER_ERROR;
+
+    const message =
+      exception instanceof HttpException
+        ? exception.message
+        : ErrorMessages.somethingWentWrong;
+
+    const responseBody = {
+      error: true,
+      statusCode: httpStatus,
+      timestamp: new Date().toISOString(),
+      path: httpAdapter.getRequestUrl(ctx.getRequest()),
+      message,
+    };
+
+    httpAdapter.reply(ctx.getResponse(), responseBody, httpStatus);
+  }
+}
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,9 @@
-import { NestFactory } from '@nestjs/core';
+import { HttpAdapterHost, NestFactory } from '@nestjs/core';
 import 'dotenv/config';
 import { ValidationPipe } from '@nestjs/common';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
+import { CommonExceptionFilter } from './common/exception-filters/common-exception.filter';
 
 declare global {
   interface BigInt {
@@ -26,10 +27,12 @@ async function bootstrap() {
     .build();
 
   const document = SwaggerModule.createDocument(app, config);
+  const httpAdapter = app.get(HttpAdapterHost);
 
   SwaggerModule.setup('/api/docs', app, document);
 
   app.useGlobalPipes(new ValidationPipe({}));
+  app.useGlobalFilters(new CommonExceptionFilter(httpAdapter));
 
   await app.listen(PORT, () => console.log('Server started on port:', PORT));
 }
