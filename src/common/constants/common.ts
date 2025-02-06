319e61ad1d9429e717694a2fe892da0b9638c7ef
Rodion Grishaev <r.grishaev@softteco.com>
2025-02-06 16:19:53 +0300

    Write logs to files.

--- src/common/constants/common.ts
+++ src/common/constants/common.ts
@@ -9,3 +9,15 @@ export enum JWT_ERROR_NAMES {
   TOKEN_EXPIRED_ERROR = 'TokenExpiredError',
   JSON_WEB_TOKEN_ERROR = 'JsonWebTokenError',
 }
+
+export const LOG_DIRECTORY_PATH = './src/logging/logs/';
+
+export enum LOG_PATH {
+  INFO = './src/logging/logs/info.csv',
+  ERROR = './src/logging/logs/error.csv',
+}
+
+export enum LOG_TYPE {
+  INFO = 'INFO',
+  ERROR = 'ERROR',
+}
--- src/logging/logging.service.ts
+++ src/logging/logging.service.ts
@@ -1,6 +1,12 @@
 import { Injectable, ConsoleLogger, Scope, HttpStatus } from '@nestjs/common';
 import { Request, Response } from 'express';
 import { ResponseBody } from 'src/common/exception-filters/common-exception.filter';
+import * as fs from 'fs';
+import {
+  LOG_DIRECTORY_PATH,
+  LOG_PATH,
+  LOG_TYPE,
+} from 'src/common/constants/common';
 
 @Injectable({ scope: Scope.TRANSIENT })
 export class LoggingService extends ConsoleLogger {
@@ -24,16 +30,47 @@ export class LoggingService extends ConsoleLogger {
         statusCode === HttpStatus.CREATED ||
         statusCode === HttpStatus.NO_CONTENT
       ) {
-        this.log(
-          `[LoggingService] ${method}: ${url}${bodyString}${queryString} ${statusCode} ${timeString}`,
-        );
+        const message = `[LoggingService] ${method}: ${url}${bodyString}${queryString} ${statusCode} ${timeString}`;
+        this.log(message);
+        this.writeLog(message, LOG_TYPE.INFO);
       }
     });
   }
 
   logError(responseBody: ResponseBody) {
     const { method, statusCode, path } = responseBody;
+    const message = `[LoggingService] ${method}: ${path} ${statusCode}`;
 
-    this.error(`[LoggingService] ${method}: ${path} ${statusCode}`);
+    this.error(message);
+    this.writeLog(message, LOG_TYPE.ERROR);
+  }
+
+  private writeLog(message: string, type: LOG_TYPE) {
+    const date = new Date().toLocaleString();
+    const path = type === LOG_TYPE.ERROR ? LOG_PATH.ERROR : LOG_PATH.INFO;
+
+    if (!fs.existsSync(LOG_DIRECTORY_PATH)) {
+      fs.mkdirSync(LOG_DIRECTORY_PATH);
+    }
+
+    try {
+      fs.appendFileSync(path, `${date} ${type} ${message}\n`);
+    } catch (error) {
+      this.log(error);
+    }
+
+    this.reduceFile(path);
+  }
+
+  private reduceFile(path: LOG_PATH) {
+    while (
+      fs.statSync(path).size >= (Number(process.env.LOG_FILE_SIZE) || 4000)
+    ) {
+      const oldCsvContent = fs.readFileSync(path).toString().split('\n');
+      oldCsvContent.shift();
+      const newCsvContent = oldCsvContent.join('\n');
+
+      fs.writeFileSync(path, newCsvContent);
+    }
   }
 }
